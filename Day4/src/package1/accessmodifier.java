359a4dd2759bef3398d40f352eb3de984ed90f03
RamalingappaBS <bsramalingappa@gmail.com>
2022-02-14 05:29:30 +0530

    First Commit

---
+++ Day4/src/package1/accessmodifier.java
@@ -0,0 +1,18 @@
+package package1;
+import package2.accessmodifier3;
+public class accessmodifier extends accessmodifier3{
+	private int id = 4;
+	public static void main(String args[]) {
+		accessmodifier2 a = new accessmodifier2();//default
+		accessmodifier3 b = new accessmodifier3();//public
+		accessmodifier c = new accessmodifier();//private
+		accessmodifier v = new accessmodifier();//protected
+
+		System.out.println(a.name);
+		System.out.println(a.city);	
+		System.out.println(b.name);
+		System.out.println(v.city);	
+		System.out.println(c.id);	
+
+	}
+}
---
+++ Day4/src/package2/accessmodifier.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class accessmodifier2 
+{
+	String name = "Ramalingappa B S";
+	String city = "Bengaluru";
+}
