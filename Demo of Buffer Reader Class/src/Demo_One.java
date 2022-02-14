c9fa57a76350354eda97dff7dd7426a1110efe44
RamalingappaBS <bsramalingappa@gmail.com>
2022-02-14 06:46:26 +0530

    First Commit changes

---
+++ Demo of Buffer Reader Class/src/Demo_One.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Demo_One
+{
+	 public static void main(String[] args) throws IOException 
+	 {   
+	        int sum;
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        System.out.println("Enter First Number: ");
+	        int a = Integer.parseInt(br.readLine());
+	        
+	        System.out.println("Enter Second Number: ");
+	        int b = Integer.parseInt(br.readLine());
+	    
+	        sum = a + b;
+	        
+	        System.out.println("Sum of these numbers: "+sum);
+	    }
+}
---
+++ Demo of Buffer Reader Class/src/Demo_Second.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Demo_Second 
+{
+	 public static void main(String[] args) throws IOException 
+	 {
+	        
+	        int sum;
+	        InputStreamReader isr = new InputStreamReader(System.in);
+	        BufferedReader br = new BufferedReader(isr);
+	        
+	        System.out.println("Enter First Number: ");
+	        int a = Integer.parseInt(br.readLine());
+	        
+	        System.out.println("Enter Second Number: ");
+	        int b = Integer.parseInt(br.readLine());
+	       
+	        sum = a + b;
+	        System.out.println("Sum of these numbers: "+sum);
+	    }
+}
