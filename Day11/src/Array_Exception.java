5ba0bdc918ebf912ba4c9ea360085d7f7df41ea1
RamalingappaBS <bsramalingappa@gmail.com>
2022-02-22 22:06:16 +0530

    Array Exception Program

---
+++ Day11/src/Array_Exception.java
@@ -0,0 +1,12 @@
+//ArrayIndexOutOfBoundsException in a Java Array.  
+public class Array_Exception 
+{
+	public static void main(String args[])
+	{  
+		int arr[]={20,40,60,80,100};  
+		for(int i=0;i<=arr.length;i++)
+		{  
+			System.out.println(arr[i]);  
+		}  
+	}
+}  
